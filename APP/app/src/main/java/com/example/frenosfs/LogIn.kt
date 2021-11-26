7fde9d7245169f70831bf71f3404238b6642f8eb
Ingrid Echeverri <ingrid.echeverri@correounivalle.edu.co>
2021-11-26 00:31:48 -0500

    Agregando pantalla de login

---
+++ APP/app/src/main/java/com/example/frenosfs/LogIn.kt
@@ -0,0 +1,17 @@
+package com.example.frenosfs
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.content.Intent
+
+
+
+
+class LogIn : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_log_in)
+
+
+    }
+}
--- APP/app/src/main/java/com/example/frenosfs/MainActivity.kt
+++ APP/app/src/main/java/com/example/frenosfs/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.frenosfs
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val log_button = findViewById<Button>(R.id.log_in)
+        log_button.setOnClickListener {
+            val intent = Intent(this, LogIn::class.java)
+            startActivity(intent)
+        }
     }
+
+
 }
